c248537644ecf091b27b5d2b232e54e2fa8d4cc4
TomazSouza <48397858+TomazSouza@users.noreply.github.com>
2022-11-28 08:21:12 -0300

    dialect

--- libs/db.js
+++ libs/db.js
@@ -25,7 +25,7 @@ module.exports = (app) => {
             config.username,
             config.password, {
                 host: "localhost",
-                dialect: "mysql",
+                dialect: "postgres",
                 operatorsAliases,
             }
         );
